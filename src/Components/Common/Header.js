c02598245ae4456458b71c33537fb8d2e505c9ab
Jack Schaeffer <jackschaeffer@Jacks-MacBook-Pro.local>
2023-05-12 22:20:44 -0500

    Adding initial files

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -2,7 +2,7 @@ function Header() {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="./">
-                <img src={require("../imgs/KRLogo.png")} width="auto" height="50" class="d-inline-block align-top" alt="" />
+                {/* <img src={require("../imgs/KRLogo.png")} width="auto" height="50" class="d-inline-block align-top" alt="" /> */}
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
