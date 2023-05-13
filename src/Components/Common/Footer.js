c886f6043352d7632ee19357ce2220fcb4de1b07
Jack Schaeffer <jackschaeffer@Jacks-MacBook-Pro.local>
2023-05-12 21:10:37 -0500

    Adding initial files

---
+++ src/Components/Common/Footer.js
@@ -0,0 +1,30 @@
+
+
+function Header() {
+    return (
+        <div class="footer bg-dark">
+
+
+		<div class="policies float-right">
+			<ul class=''>
+				<li><a href="#">Subscribe</a></li>
+				<li><a href="#">Cookie Policy</a></li>
+				<li><a href="#">Your Privacy</a></li>
+				<li><a href="#">About</a></li>
+			</ul>
+		</div>
+
+
+
+		<div class="footer-logo-div float-lwft">
+			<img src="./styles/footerIcon.png" class="footer-logo d-inline-block align-top" alt=""/>
+		</div>
+
+ <div class="clear"></div>
+
+	</div>
+
+    );
+}
+
+export default Header;
