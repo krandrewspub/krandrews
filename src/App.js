fd19d50de5bb2b28837aeb63b907c2ee2f1837ba
Jack Schaeffer <jackschaeffer@Jacks-MacBook-Pro.local>
2023-05-12 23:11:03 -0500

    Adding initial files

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
 
                 <div class="row books-show">
                   <div class="col-sm-4">
-                    <img class="top-content-img" src="../imgs/book.png" />
+                    <img class="top-content-img" src="./imgs/book.png" />
                   </div>
                   <div class="col-sm-8 text-center">
                     <div class="card-body">
@@ -108,7 +108,7 @@ function App() {
               <div class="col-md-2"></div>
 
               <div class="col-md-3 author-pic">
-                <img src="../imgs/AuthorPic.png" />
+                <img src="./imgs/AuthorPic.png" />
                 <h6 class="garamond">K.R. Andrews</h6>
                 <p class="garamond">Lorem ipsum glara fernto sdfadf sadf dsfasdf asdf </p>
               </div>
